57cacd8deead7212585ce4673af61c73972b0342
ecrespo66 <enrique.crespo.debenito@gmail.com>
2021-01-24 18:21:59 +0100

    Robot_famework first Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import json
+import traceback
+
+import robot
+
+if __name__ == "__main__":
+    args = json.loads(str(sys.argv[1]))
+    try:
+        Robot = robot.Main(args)
+        Robot.runRobot()
+    except:
+        for line in traceback.format_exc().splitlines():
+            Robot.Log.systemException(str(line))
+        Robot.Log.systemException("[Execution Fail]")
